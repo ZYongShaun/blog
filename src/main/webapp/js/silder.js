260f678f627280c3ff698072acecbc74fdddd0ab
shaun.zyong <shaun.zyong@gmail.com>
2016-09-27 10:47:54 +0800

    modi

--- src/main/webapp/js/silder.js
+++ src/main/webapp/js/silder.js
@@ -1,6 +1,10 @@
-var obj=null;
-var As=document.getElementById('nav').getElementsByTagName('a');
-obj = As[0];
-for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
-obj=As[i];}
-obj.id='nav_current'
+$(function () {
+    var currentUrl = location.href;
+    var navList = $("#nav").find('a');
+    navList.first().addClass('nav_current');
+    navList.each(function (index, item) {
+         if(currentUrl.indexOf(item.href) != -1){
+             $(item).addClass('nav_current');
+         }
+     })
+});
